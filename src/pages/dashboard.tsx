197e5c642757bd818093b053a6721c1390f7ac3e
Joseph Petrillo <jpetrillo119@gmail.com>
2022-08-16 00:43:43 -0400

    protect dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,6 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
+import { unstable_getServerSession as getServerSession } from "next-auth";
+import { authOptions as nextAuthOptions } from "./api/auth/[...nextauth]";
 
 const Dashboard: NextPage = () => {
   return (
@@ -6,6 +8,27 @@ const Dashboard: NextPage = () => {
       <p>This will be the dashboard page.</p>
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getServerSession(
+    context.req,
+    context.res,
+    nextAuthOptions
+  );
+
+  if (session) {
+    return {
+      props: {}, // will be passed to the page component as props
+    };
+  } else {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
 };
 
 export default Dashboard;
